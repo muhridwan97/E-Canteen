6073363e274741a8ca04fabf5f67666a80162878
muhridwan97 <muh.ridwan97@gmail.com>
2017-03-10 16:28:10 +0700

    uhuy

---
+++ e-canteen/MenuPembeli/stand2.php
@@ -0,0 +1,18 @@
+<html>
+	<title>
+    	Stand-2
+    </title>
+    <head>
+		<link rel="stylesheet" type="text/css" href="../stylelogin.css">
+	</head>
+<body>
+<ul>
+  <li><a class="active"  href="../index.html">Home</a></li>
+  <li><a href="#news">Notification</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a href="#about">About</a></li>
+  <li style="float:right"><a href="#about">Log Out</a></li>
+</ul>
+    
+</body>
+</html>
